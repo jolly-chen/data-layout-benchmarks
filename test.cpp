7e7dc1be6d5914676547f75b9e76e6353aa6e095
Jolly Chen <jolly.chen@student.uva.nl>
2025-12-10 13:59:27 +0100

    Add Particle + transformation facility
    Squashed commit of the following:
    
    commit f9f0177c7c767e6bd762f6277e7c85740fcec395
    Author: Jolly Chen <jolly.chen@student.uva.nl>
    Date:   Wed Dec 10 13:55:55 2025 +0100
    
        Change test.cpp into a header file
    
    commit 23d1598ade1dadccf6d6ef260c5e99760d4ea449
    Author: Jolly Chen <jolly.chen@student.uva.nl>
    Date:   Wed Dec 10 13:45:32 2025 +0100
    
        Implement TransformStruct taking SplitOps
    
    commit 38b1064c7891d300dc58b4f9777175486ea5ecbb
    Author: Jolly Chen <jolly.chen@student.uva.nl>
    Date:   Tue Dec 9 14:20:55 2025 +0100
    
        Define modifications to Particle with reflection
    
        Uses C++ reflection to define variations of the base Particle struct.
        TransformedParticle takes a SplitOp template argument that defines
        which data members of Particle it should take over. The members
        can be reordered. Multiple SplitOps can be passed to generate
        multiple structures that contain a subset of Particle's data
        members.
    
        Contains failed attempts in comments for future reference

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include "datastructures.h"
+#include <iostream>
+
+int main() {
+  TransformedParticle<SplitOp({0, 1, 2}).data()> f;
+  f.id = 11;
+  f.pt = 12;
+  f.eta = 10;
+  std::cout << "id: " << f.id << ", pt: " << f.pt << ", eta: " << f.eta << "\n";
+
+  TransformedParticle<SplitOp({3, 4}).data()> g;
+  g.phi = 1.5;
+  g.e = 20.0;
+  std::cout << "phi: " << g.phi << ", e: " << g.e << "\n";
+}
