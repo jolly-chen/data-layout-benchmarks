0c00fe953c206bad801e4918c45735c3c368355f
Jolly Chen <jolly.chen@student.uva.nl>
2025-12-10 13:55:55 +0100

    Change test.cpp into a header file

--- test.cpp
+++ test.cpp
@@ -1,54 +1,5 @@
-// Compile with clang-p2996
-#include <array>
-#include <cassert>
-#include <experimental/meta>
+#include "datastructures.h"
 #include <iostream>
-
-consteval auto SplitOp(std::vector<int> indices) {
-  return define_static_array(indices);
-}
-
-template <typename S>
-consteval auto get_member_specs(std::span<const int> indices) {
-  auto members =
-      nonstatic_data_members_of(^^S, std::meta::access_context::unchecked());
-
-  std::vector<std::meta::info> specs;
-  for (auto index : indices) {
-    auto mem_descr = data_member_spec(type_of(members[index]),
-                                      {.name = identifier_of(members[index])});
-    specs.push_back(mem_descr);
-  }
-  return specs;
-}
-
-// clang-format off
-template <typename In, template <auto> typename Out, typename... SplitOps>
-consteval void TransformStruct(SplitOps... ops) {
-  (define_aggregate(substitute(^^Out, { std::meta::reflect_constant_array(ops)}),
-                    get_member_specs<In>(ops)),
-   ...);
-};
-// clang-format on
-
-///////////////
-
-struct Particle {
-  int id;
-  double pt, eta, phi, e;
-  char charge;
-  std::array<std::array<double, 3>, 3> posCovMatrix;
-};
-
-template <auto... Members> struct TransformedParticle;
-
-consteval void GenerateTransformedParticles() {
-
-  TransformStruct<Particle, TransformedParticle>(SplitOp({0, 1, 2}),
-                                                 SplitOp({3, 4}));
-}
-
-consteval { GenerateTransformedParticles(); }
 
 int main() {
   TransformedParticle<SplitOp({0, 1, 2}).data()> f;
