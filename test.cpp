23d1598ade1dadccf6d6ef260c5e99760d4ea449
Jolly Chen <jolly.chen@student.uva.nl>
2025-12-10 13:45:32 +0100

    Implement TransformStruct taking SplitOps

--- test.cpp
+++ test.cpp
@@ -22,6 +22,17 @@ consteval auto get_member_specs(std::span<const int> indices) {
   return specs;
 }
 
+// clang-format off
+template <typename In, template <auto> typename Out, typename... SplitOps>
+consteval void TransformStruct(SplitOps... ops) {
+  (define_aggregate(substitute(^^Out, { std::meta::reflect_constant_array(ops)}),
+                    get_member_specs<In>(ops)),
+   ...);
+};
+// clang-format on
+
+///////////////
+
 struct Particle {
   int id;
   double pt, eta, phi, e;
@@ -29,88 +40,17 @@ struct Particle {
   std::array<std::array<double, 3>, 3> posCovMatrix;
 };
 
-// template <auto Ts>
-template <auto Members> struct TransformedParticle;
+template <auto... Members> struct TransformedParticle;
 
-/////////////// consteval func version
+consteval void GenerateTransformedParticles() {
 
-// template <typename S, auto... SplitOps>
-// consteval void TransformStruct() {
-//   static_assert(
-//       template_arguments_of(^^TransformStruct<S, SplitOps...>).size() ==
-//       sizeof...(SplitOps) + 1);
-
-//   (define_aggregate(^^TransformedParticle<SplitOps>,
-//   get_member_specs<Particle>(SplitOps)), ...);
-// };
-
-/////////////// initializer list version
-
-// template <typename S, typename... SplitOps>
-// consteval void TransformStruct(std::initializer_list<SplitOps>... ops) {
-//   (define_aggregate(
-//        ^^TransformedParticle<std::define_static_array(std::vector{ops}).data()>,
-//        get_member_specs<Particle>(ops)),
-//    ...);
-// };
-
-/////////////// std::vector version
-
-// template <typename S, typename... SplitOps>
-//   requires requires { (std::same_as<SplitOps, std::vector<int>> && ...); }
-// consteval void TransformStruct(SplitOps... ops) {
-//   (define_aggregate(^^TransformedParticle<int>,
-//                     get_member_specs<Particle>(ops)),
-//    ...);
-// };
-
-/////////////// SplitOp version
-
-template <typename S, typename... SplitOps>
-consteval void TransformStruct(SplitOps... ops) {
-  (define_aggregate(
-       //  ^^TransformedParticle<reflect_constant(ops[std::make_index_sequence<ops.size()>{}])...>,
-       //  ^^TransformedParticle<std::meta::reflect_constant_array(ops)>,
-       substitute(^^TransformedParticle,
-                  {
-                      std::meta::reflect_constant_array(ops)}),
-       get_member_specs<Particle>(ops)),
-   ...);
-};
-
-/////////////// struct version
-
-// template <typename S, typename... SplitOps>
-// struct TransformStruct {
-//   consteval {
-//     static_assert(
-//         template_arguments_of(^^TransformStruct<S, SplitOps...>).size() ==
-//         sizeof...(SplitOps) + 1);
-
-//     auto members = nonstatic_data_members_of(
-//         ^^Particle, std::meta::access_context::unchecked());
-//     (define_aggregate(^^TransformedParticle<SplitOps>, {}), ...);
-//   }
-// };
-
-///////////////
-
-consteval {
-  // TransformStruct<Particle, SplitOp({0, 1, 2}), SplitOp({3, 4})>();
-  // TransformStruct<Particle>(std::vector{0, 1, 2}, std::vector{3, 4});
-  // TransformStruct<Particle>({0, 1, 2}, {3, 4});
-  TransformStruct<Particle>(SplitOp({0, 1, 2}), SplitOp({3, 4}));
+  TransformStruct<Particle, TransformedParticle>(SplitOp({0, 1, 2}),
+                                                 SplitOp({3, 4}));
 }
 
+consteval { GenerateTransformedParticles(); }
+
 int main() {
-  // TransformedParticle<reflec{1,2,3}> f;
-  // TransformedParticle<SplitOp({0, 1, 2}).data()> f;
-  // TransformStruct<Particle>({0, 1, 2}, {3, 4});
-
-  // using tp = TransformStruct<Particle, SplitOp({0, 1, 2}), SplitOp({3, 4})> ;
-  // TransformStruct<Particle>(std::vector{0, 1, 2}, std::vector{3, 4});
-  // TransformStruct<Particle>(SplitOp({0, 1, 2}), SplitOp({3, 4}));
-
   TransformedParticle<SplitOp({0, 1, 2}).data()> f;
   f.id = 11;
   f.pt = 12;
