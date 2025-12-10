510ccbe88738686e5a7aeb293da7455dce77825b
Jolly Chen <jolly.chen@student.uva.nl>
2025-12-10 13:55:55 +0100

    Change test.cpp into a header file

--- datastructures.h
+++ datastructures.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:479905d9d20319c1c7e3bf5b1cacd68b04ea9bc67c2b4c66902f142a88b1c67d
-size 1082842
+oid sha256:966916baaa21ecb40357fcc42a63353c071084aad5e8cb13671c233ce2745494
+size 1425
