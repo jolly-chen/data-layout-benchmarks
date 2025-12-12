90966f54a722bb887fcf27f0073ce2ed35273e8a
Jolly Chen <jolly.chen@student.uva.nl>
2025-12-12 14:21:14 +0100

    Fix compilation errors

--- benchmarks.h
+++ benchmarks.h
@@ -9,26 +9,19 @@ inline double ComputeInvariantMass(const T &pt1, const T &eta1, const T &phi1,
                                    const T &e1, const T &pt2, const T &eta2,
                                    const T &phi2, const T &e2) {
   // Conversion from (pt, eta, phi, e) to (x, y, z, mass) coordinate system
-  const auto x1 = pt1[i] * std::cos(phi1[i]);
-  const auto y1 = pt1[i] * std::sin(phi1[i]);
-  const auto z1 = pt1[i] * std::sinh(eta1[i]);
-  const auto m1 =
-      std::sqrt(e1[i] * e1[i] -
-                (pt1[i] * pt1[i] * std::cosh(eta1[i]) * std::cosh(eta1[i])));
+  const auto x1 = pt1 * std::cos(phi1);
+  const auto y1 = pt1 * std::sin(phi1);
+  const auto z1 = pt1 * std::sinh(eta1);
+  const auto m1_sq = e1 * e1 - (pt1 * pt1 * std::cosh(eta1) * std::cosh(eta1));
 
-  const auto x2 = pt2[i] * std::cos(phi2[i]);
-  const auto y2 = pt2[i] * std::sin(phi2[i]);
-  const auto z2 = pt2[i] * std::sinh(eta2[i]);
-  const auto m2 =
-      std::sqrt(e2[i] * e2[i] -
-                (pt2[i] * pt2[i] * std::cosh(eta2[i]) * std::cosh(eta2[i])));
+  const auto x2 = pt2 * std::cos(phi2);
+  const auto y2 = pt2 * std::sin(phi2);
+  const auto z2 = pt2 * std::sinh(eta2);
+  const auto m2_sq = e2 * e2 - (pt2 * pt2 * std::cosh(eta2) * std::cosh(eta2));
 
   // Numerically stable computation of Invariant Masses
   const auto p1_sq = x1 * x1 + y1 * y1 + z1 * z1;
   const auto p2_sq = x2 * x2 + y2 * y2 + z2 * z2;
-
-  const auto m1_sq = m1 * m1;
-  const auto m2_sq = m2 * m2;
 
   const auto r1 = m1_sq / p1_sq;
   const auto r2 = m2_sq / p2_sq;
@@ -67,28 +60,23 @@ inline double InvariantMassSequential(const std::vector<T> &v1,
   const size_t n = v1.size();
 
   for (size_t i = 0; i < n; i++) {
-    results[i] = ComputeInvariantMass(
-        MEMBER_ACCESS(v1, pt, i), MEMBER_ACCESS(v1, eta, i),
-        MEMBER_ACCESS(v1, phi, i), MEMBER_ACCESS(v1, e, i),
-        MEMBER_ACCESS(v2, pt, i), MEMBER_ACCESS(v2, eta, i),
-        MEMBER_ACCESS(v2, phi, i), MEMBER_ACCESS(v2, e, i));
+    results[i] = ComputeInvariantMass(v1[i].pt, v1[i].eta, v1[i].phi, v1[i].e,
+                                      v2[i].pt, v2[i].eta, v2[i].phi, v2[i].e);
   }
 }
 
 template <typename T>
-inline double InvariantMassRandom(const std::vector<T> &v1,
-                                  const std::vector<T> &v2, size_t n,
+inline void InvariantMassRandom(const std::vector<T> &v1,
+                                  const std::vector<T> &v2,
                                   std::vector<double> &results) {
   assert(v1.size() == v2.size());
   const size_t n = v1.size();
 
   for (size_t i = 0; i < n; i++) {
     size_t idx = rand() % n;
-    results[i] = ComputeInvariantMass(
-        MEMBER_ACCESS(v1, pt, idx), MEMBER_ACCESS(v1, eta, idx),
-        MEMBER_ACCESS(v1, phi, idx), MEMBER_ACCESS(v1, e, idx),
-        MEMBER_ACCESS(v2, pt, idx), MEMBER_ACCESS(v2, eta, idx),
-        MEMBER_ACCESS(v2, phi, idx), MEMBER_ACCESS(v2, e, idx));
+    results[i] =
+        ComputeInvariantMass(v1[idx].pt, v1[idx].eta, v1[idx].phi, v1[idx].e,
+                             v2[idx].pt, v2[idx].eta, v2[idx].phi, v2[idx].e);
   }
 }
 
@@ -96,17 +84,15 @@ template <typename T>
 inline double DeltaR2(const T &eta1, const T &phi1, const T &eta2,
                       const T &phi2) {
 
-  for (size_t i = 0; i < n; i++) {
-    const auto deta = eta1 - eta2;
-    const auto dphi = phi1 - phi2;
-    const auto r = std::fmod(dphi + M_PI, 2 * M_PI);
-    if (r < -M_PI) {
-      return = deta * deta + (r + 2 * M_PI) * (r + 2 * M_PI);
-    } else if (r > M_PI) {
-      return = deta * deta + (r - 2 * M_PI) * (r - 2 * M_PI);
-    } else {
-      return = deta * deta + r * r;
-    }
+  const auto deta = eta1 - eta2;
+  const auto dphi = phi1 - phi2;
+  const auto r = std::fmod(dphi + M_PI, 2 * M_PI);
+  if (r < -M_PI) {
+    return deta * deta + (r + 2 * M_PI) * (r + 2 * M_PI);
+  } else if (r > M_PI) {
+    return deta * deta + (r - 2 * M_PI) * (r - 2 * M_PI);
+  } else {
+    return deta * deta + r * r;
   }
 }
 
@@ -115,9 +101,7 @@ double DeltaR2Pairwise(const std::vector<T> &v1, const std::vector<T> &v2,
                        size_t n, std::vector<double> &results) {
   for (size_t i = 0; i < v1.size(); i++) {
     for (size_t j = 0; j < v2.size(); j++) {
-      results[i * n + j] =
-          DeltaR2(MEMBER_ACCESS(v1, eta, i), MEMBER_ACCESS(v1, phi, i),
-                  MEMBER_ACCESS(v2, eta, j), MEMBER_ACCESS(v2, phi, j));
+      results[i * n + j] = DeltaR2(v1[i].eta, v1[i].phi, v2[j].eta, v2[j].phi);
     }
   }
 }
