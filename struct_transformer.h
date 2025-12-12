abc63879090837ff41c2e1f0436ac3b091171476
Jolly Chen <jolly.chen@student.uva.nl>
2025-12-12 18:37:11 +0100

    WIP: same access interface for partitions

--- struct_transformer.h
+++ struct_transformer.h
@@ -18,7 +18,7 @@ consteval auto get_member_specs(std::span<const int> indices) {
 
   std::vector<std::meta::info> specs;
   for (auto index : indices) {
-    auto mem_descr = data_member_spec(type_of(members[index]),
+    auto mem_descr = data_member_spec(remove_cvref(type_of(members[index])),
                                       {.name = identifier_of(members[index])});
     specs.push_back(mem_descr);
   }
@@ -27,11 +27,44 @@ consteval auto get_member_specs(std::span<const int> indices) {
 
 // clang-format off
 template <typename In, template <auto> typename Out, typename... SplitOps>
-consteval void TransformStruct(SplitOps... ops) {
+consteval void SplitStruct(SplitOps... ops) {
   (define_aggregate(substitute(^^Out, { std::meta::reflect_constant_array(ops)}),
                     get_member_specs<In>(ops)),
    ...);
 };
 // clang-format on
+
+// template <typename Original, typename P1, typename P2>
+// struct ParitionedContainer {
+//   P1 *p1;
+//   P2 *p2;
+//   size_t n;
+
+//   ParitionedContainer(size_t n) : n(n) {
+//     p1 = std::allocator<P1>().allocate(n);
+//     p2 = std::allocator<P2>().allocate(n);
+//   }
+
+//   inline Original operator[](size_t index) const {
+//     return {p1[index].pt, p1[index].eta, p1[index].phi, p2[index].e};
+//   }
+
+//   size_t size() const {
+//     // Assuming both partitions have the same size
+//     return n;
+//   }
+// };
+
+// template <typename Original, typename T> struct PartitionedContainer {
+//   struct Partitions;
+
+//   consteval {
+//     define_aggregate(^^Partitions, {data_member_spec(^^T)});
+//   }
+
+//   PartitionedContainer(size_t n) {}
+
+//   // inline Original operator[](size_t index) { return Original{}; }
+// };
 
 #endif // STRUCT_TRANSFORMER_H
