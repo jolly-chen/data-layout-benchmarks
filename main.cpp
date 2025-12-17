a483c0c862cdb39826c153d77a01db61332817cd
Jolly Chen <jolly.chen@student.uva.nl>
2025-12-17 17:21:08 +0100

    Read vector data from file

--- main.cpp
+++ main.cpp
@@ -2,27 +2,63 @@
 #include "datastructures.h"
 #include <chrono>
 #include <iostream>
+#include <fstream>
 
 using Clock = std::chrono::high_resolution_clock;
 
-template <typename T, typename... Partitions>
-void RunInvariantMassRandom(size_t n) {
-  using PC = PartitionedContainer<T, Partitions...>;
-  PC v1(n), v2(n);
+template <typename Container>
+void read_data(Container &v, std::string filename) {
+  std::ifstream is(filename);
+  if (is.is_open()) {
+    std::string line;
+    for (size_t i = 0; i < v.size(); ++i) {
+      getline(is, line);
+      std::stringstream ss(line);
+      std::string token;
+      std::vector<std::string> temp;
 
-  std::vector<double> results(n);
+      getline(ss, token, ',');
+      v[i].pt = std::stof(token);
+      getline(ss, token, ',');
+      v[i].eta = std::stof(token);
+      getline(ss, token, ',');
+      v[i].phi = std::stof(token);
+      getline(ss, token, ',');
+      v[i].e = std::stof(token);
+    }
+    is.close();
+  }
+}
+
+template <typename Container, typename BenchmarkFunc>
+void RunBenchmark(size_t in_size, size_t out_size, BenchmarkFunc func) {
+  Container v1(in_size), v2(in_size);
+  read_data(v1, "/data/data-layout-benchmarks/data/100k.csv");
+  read_data(v2, "/data/data-layout-benchmarks/data/100k.csv");
+
+  std::vector<double> results(out_size);
 
   auto start = Clock::now();
-  InvariantMassRandom(v1, v2, results);
+  func(v1, v2, results);
   auto end = Clock::now();
 
   std::chrono::duration<double> elapsed = end - start;
   std::cout << "Elapsed time: " << elapsed.count() << " seconds\n";
 }
 
-int main() {
-  RunInvariantMassRandom<Particle, SubParticle<SplitOp({0, 1, 2}).data()>, SubParticle<SplitOp({3}).data()>>(10000000);
-  return 0;
+template <typename Container> void RunAllBenchmarks(size_t n) {
+  std::cout << "Running InvariantMassRandom\n";
+  RunBenchmark<Container>(n, n, kernels::InvariantMassRandom<Container>);
+
+  std::cout << "Running InvariantMassSequential\n";
+  RunBenchmark<Container>(n, n, kernels::InvariantMassSequential<Container>);
+
+  std::cout << "Running DeltaR2Pairwise\n";
+  RunBenchmark<Container>(n, n*n, kernels::DeltaR2Pairwise<Container>);
 }
 
-// https://godbolt.org/z/xhTMKPsTK
+int main() {
+  RunAllBenchmarks<PartitionedContainer<
+      Particle, SubParticle<SplitOp({0, 1, 2, 3}).data()>>>(10);
+  return 0;
+}
