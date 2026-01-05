1833d6ea5ded7fb0851112c43549b291a01c6193
Jolly Chen <jolly.chen@cern.ch>
2026-01-05 14:00:06 +0100

    Fix random indices and print agg runtimes
    InvariantMassRandom was accessing array indices
    that were out of bounds
    
    Add command line option parser

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include "benchmarks.h"
 #include "datastructures.h"
 
-#include <string>
+#include <algorithm>
 #include <experimental/meta>
 #include <iostream>
-#include <algorithm>
 #include <sstream>
+#include <string>
 
 #include <chrono>
 #include <fstream>
@@ -14,30 +14,32 @@
 #include <random>
 
 using Clock = std::chrono::high_resolution_clock;
+using unit = std::milli;
 
-size_t max_results_size = 1048576; // 2^20
+size_t max_results_size = 65536; // 2^16
 
 struct FileOpts {
-  std::string input = "";            // Option "--input <string>"
-  std::string input2 = "";           // Option "--input2 <string>"
-  std::string output = "";           // Option "--output <string>"
-  std::string validation = "";       // Option "--validation <string>"
+  std::string input = "";      // Option "--input <string>"
+  std::string input2 = "";     // Option "--input2 <string>"
+  std::string output = "";     // Option "--output <string>"
+  int repetitions = 5;         // Option "--repetitions <int>"
+  std::string validation = ""; // Option "--validation <string>"
 };
 
 FileOpts opts;
 std::ostream *output;
 
 /* Parse command-line options. */
-template<typename Opts>
+template <typename Opts>
 auto parse_options(std::span<std::string_view const> args) -> Opts {
   Opts opts;
 
   constexpr auto ctx = std::meta::access_context::current();
-  template for (constexpr auto dm : define_static_array(nonstatic_data_members_of(^^Opts, ctx))) {
-    auto it = std::find_if(args.begin(), args.end(),
-      [](std::string_view arg){
-        return arg.starts_with("--") && arg.substr(2) == identifier_of(dm);
-      });
+  template for (constexpr auto dm :
+                define_static_array(nonstatic_data_members_of(^^Opts, ctx))) {
+    auto it = std::find_if(args.begin(), args.end(), [](std::string_view arg) {
+      return arg.starts_with("--") && arg.substr(2) == identifier_of(dm);
+    });
 
     if (it == args.end()) {
       // no option provided, use default
@@ -63,7 +65,8 @@ auto parse_options(std::span<std::string_view const> args) -> Opts {
   return opts;
 }
 
-/* Read Lorentzvector (pt, eta, phi, e) data from the given CSV file into the container. */
+/* Read Lorentzvector (pt, eta, phi, e) data from the given CSV file into the
+ * container. */
 template <typename Container>
 void ReadData(Container &v, std::string filename) {
   std::ifstream is(filename);
@@ -92,40 +95,61 @@ void ReadData(Container &v, std::string filename) {
     }
     is.close();
   } else {
-    throw std::runtime_error("Failed to open file " + filename + " for reading");
+    throw std::runtime_error("Failed to open file " + filename +
+                             " for reading");
   }
 }
 
 template <typename Container, std::meta::info BenchmarkFunc,
           typename... ExtraArgs>
 void RunBenchmark(size_t in_size, size_t out_size, ExtraArgs... extra_args) {
-  Container v1(in_size), v2(in_size);
-  ReadData(v1, opts.input);
-  ReadData(v2, opts.input2);
+  std::vector<double> measured_times;
 
-  // Cap the results size to avoid excessive memory usage
-  std::vector<double> results(std::min(out_size, max_results_size));
+  for (int _ = 0; _ < opts.repetitions; ++_) {
+    Container v1(in_size), v2(in_size);
+    ReadData(v1, opts.input);
+    ReadData(v2, opts.input2);
 
-  auto start = Clock::now();
-  [:substitute(BenchmarkFunc, {^^Container}):](v1, v2, results, extra_args...);
-  auto end = Clock::now();
+    // Cap the results size to avoid excessive memory usage
+    std::vector<double> results(std::min(out_size, max_results_size));
+
+    auto start = Clock::now();
+    [:substitute(BenchmarkFunc, {^^Container}):](v1, v2, results,
+                                                 extra_args...);
+    auto end = Clock::now();
+
+    std::chrono::duration<double, unit> elapsed = end - start;
+    measured_times.push_back(elapsed.count());
+  }
 
   // Print configuration and timing information in csv format
-  std::chrono::duration<double, std::milli> elapsed = end - start;
+  double min = *std::ranges::min_element(measured_times);
+  double max = *std::ranges::max_element(measured_times);
+  double avg = std::reduce(measured_times.begin(), measured_times.end(), 0.0) /
+               measured_times.size();
+  double stddev =
+      std::sqrt(std::reduce(measured_times.begin(), measured_times.end(), 0.0,
+                            [avg](double acc, double t) {
+                              return acc + (t - avg) * (t - avg);
+                            }) /
+                measured_times.size());
   *output << identifier_of(BenchmarkFunc) << ","
           << Container::get_partitions_string() << "," << in_size << ","
-          << sizeof(Container) << "," << elapsed.count() << std::endl;
+          << sizeof(Container) << "," << display_string_of(dealias(^^unit))
+          << "," << min << "," << max << "," << avg << "," << stddev
+          << std::endl;
 }
 
 /* Run all benchmarks defined in benchmarks.h. */
 template <typename Container> void RunAllBenchmarks(size_t n) {
+  RunBenchmark<Container, ^^kernels::InvariantMassSequential>(n, n);
+
   std::vector<size_t> indices(n);
-  std::iota(begin(indices), end(indices), n);
-  std::mt19937 rng(std::random_device{}());
+  std::iota(begin(indices), end(indices), 0);
+  std::mt19937 rng(123);
   std::shuffle(begin(indices), end(indices), rng);
   RunBenchmark<Container, ^^kernels::InvariantMassRandom>(n, n, indices);
 
-  RunBenchmark<Container, ^^kernels::InvariantMassSequential>(n, n);
   RunBenchmark<Container, ^^kernels::DeltaR2Pairwise>(n, n * n);
 }
 
@@ -142,21 +166,23 @@ template <typename T> std::vector<size_t> GetProblemSizes() {
   // Fits in L1 Cache
   sizes.push_back(topo->cacheLevels[0].size / sizeof(T) / 3);
   // Does not fit in any cache
-  sizes.push_back(topo->cacheLevels[topo->numCacheLevels - 1].size * 2 /
-                  sizeof(T));
-
+  sizes.push_back(topo->cacheLevels[topo->numCacheLevels - 1].size / sizeof(T));
+  
   return sizes;
 }
 
 int main(int argc, char *argv[]) {
-  opts = parse_options<FileOpts>(std::vector<std::string_view>(argv+1, argv+argc));
+  opts = parse_options<FileOpts>(
+      std::vector<std::string_view>(argv + 1, argv + argc));
   std::ofstream output_file(opts.output);
   if (output_file.is_open()) {
     output = &output_file;
   } else {
     output = &std::cout;
   }
-  *output << "benchmark,partitions,problem_size,container_byte_size,time_ms\n";
+  *output << "benchmark,partitions,problem_size,container_byte_size,time_ratio,"
+             "min,max,"
+             "avg,stddev\n";
 
   for (size_t n : GetProblemSizes<Particle>()) {
     RunAllBenchmarks<PartitionedContainer<
@@ -244,23 +270,29 @@ int main(int argc, char *argv[]) {
         ParticleRef, SubParticle<SplitOp({3, 1, 0}).data()>,
         SubParticle<SplitOp({2}).data()>>>(n);
     RunAllBenchmarks<
-        PartitionedContainer<ParticleRef, SubParticle<SplitOp({0, 1}).data()>,
+        PartitionedContainer<ParticleRef, SubParticle<SplitOp({0,
+        1}).data()>,
                              SubParticle<SplitOp({2, 3}).data()>>>(n);
     RunAllBenchmarks<
-        PartitionedContainer<ParticleRef, SubParticle<SplitOp({1, 0}).data()>,
+        PartitionedContainer<ParticleRef, SubParticle<SplitOp({1,
+        0}).data()>,
                              SubParticle<SplitOp({2, 3}).data()>>>(n);
     RunAllBenchmarks<
-        PartitionedContainer<ParticleRef, SubParticle<SplitOp({1, 0}).data()>,
+        PartitionedContainer<ParticleRef, SubParticle<SplitOp({1,
+        0}).data()>,
                              SubParticle<SplitOp({2, 3}).data()>>>(n);
     RunAllBenchmarks<
-        PartitionedContainer<ParticleRef, SubParticle<SplitOp({1, 0}).data()>,
+        PartitionedContainer<ParticleRef, SubParticle<SplitOp({1,
+        0}).data()>,
                              SubParticle<SplitOp({3, 2}).data()>>>(n);
     RunAllBenchmarks<PartitionedContainer<
         ParticleRef, SubParticle<SplitOp({0, 1}).data()>,
-        SubParticle<SplitOp({2}).data()>, SubParticle<SplitOp({3}).data()>>>(n);
+        SubParticle<SplitOp({2}).data()>,
+        SubParticle<SplitOp({3}).data()>>>(n);
     RunAllBenchmarks<PartitionedContainer<
         ParticleRef, SubParticle<SplitOp({1, 0}).data()>,
-        SubParticle<SplitOp({2}).data()>, SubParticle<SplitOp({3}).data()>>>(n);
+        SubParticle<SplitOp({2}).data()>,
+        SubParticle<SplitOp({3}).data()>>>(n);
     RunAllBenchmarks<PartitionedContainer<
         ParticleRef, SubParticle<SplitOp({0, 2, 3}).data()>,
         SubParticle<SplitOp({1}).data()>>>(n);
@@ -280,34 +312,44 @@ int main(int argc, char *argv[]) {
         ParticleRef, SubParticle<SplitOp({3, 2, 0}).data()>,
         SubParticle<SplitOp({1}).data()>>>(n);
     RunAllBenchmarks<
-        PartitionedContainer<ParticleRef, SubParticle<SplitOp({0, 2}).data()>,
+        PartitionedContainer<ParticleRef, SubParticle<SplitOp({0,
+        2}).data()>,
                              SubParticle<SplitOp({1, 3}).data()>>>(n);
     RunAllBenchmarks<
-        PartitionedContainer<ParticleRef, SubParticle<SplitOp({2, 0}).data()>,
+        PartitionedContainer<ParticleRef, SubParticle<SplitOp({2,
+        0}).data()>,
                              SubParticle<SplitOp({1, 3}).data()>>>(n);
     RunAllBenchmarks<
-        PartitionedContainer<ParticleRef, SubParticle<SplitOp({2, 0}).data()>,
+        PartitionedContainer<ParticleRef, SubParticle<SplitOp({2,
+        0}).data()>,
                              SubParticle<SplitOp({1, 3}).data()>>>(n);
     RunAllBenchmarks<
-        PartitionedContainer<ParticleRef, SubParticle<SplitOp({2, 0}).data()>,
+        PartitionedContainer<ParticleRef, SubParticle<SplitOp({2,
+        0}).data()>,
                              SubParticle<SplitOp({3, 1}).data()>>>(n);
     RunAllBenchmarks<PartitionedContainer<
         ParticleRef, SubParticle<SplitOp({0, 2}).data()>,
-        SubParticle<SplitOp({1}).data()>, SubParticle<SplitOp({3}).data()>>>(n);
+        SubParticle<SplitOp({1}).data()>,
+        SubParticle<SplitOp({3}).data()>>>(n);
     RunAllBenchmarks<PartitionedContainer<
         ParticleRef, SubParticle<SplitOp({2, 0}).data()>,
-        SubParticle<SplitOp({1}).data()>, SubParticle<SplitOp({3}).data()>>>(n);
+        SubParticle<SplitOp({1}).data()>,
+        SubParticle<SplitOp({3}).data()>>>(n);
     RunAllBenchmarks<
-        PartitionedContainer<ParticleRef, SubParticle<SplitOp({0, 3}).data()>,
+        PartitionedContainer<ParticleRef, SubParticle<SplitOp({0,
+        3}).data()>,
                              SubParticle<SplitOp({1, 2}).data()>>>(n);
     RunAllBenchmarks<
-        PartitionedContainer<ParticleRef, SubParticle<SplitOp({3, 0}).data()>,
+        PartitionedContainer<ParticleRef, SubParticle<SplitOp({3,
+        0}).data()>,
                              SubParticle<SplitOp({1, 2}).data()>>>(n);
     RunAllBenchmarks<
-        PartitionedContainer<ParticleRef, SubParticle<SplitOp({3, 0}).data()>,
+        PartitionedContainer<ParticleRef, SubParticle<SplitOp({3,
+        0}).data()>,
                              SubParticle<SplitOp({1, 2}).data()>>>(n);
     RunAllBenchmarks<
-        PartitionedContainer<ParticleRef, SubParticle<SplitOp({3, 0}).data()>,
+        PartitionedContainer<ParticleRef, SubParticle<SplitOp({3,
+        0}).data()>,
                              SubParticle<SplitOp({2, 1}).data()>>>(n);
     RunAllBenchmarks<
         PartitionedContainer<ParticleRef, SubParticle<SplitOp({0}).data()>,
@@ -329,34 +371,36 @@ int main(int argc, char *argv[]) {
                              SubParticle<SplitOp({3, 2, 1}).data()>>>(n);
     RunAllBenchmarks<PartitionedContainer<
         ParticleRef, SubParticle<SplitOp({0}).data()>,
-        SubParticle<SplitOp({1, 2}).data()>, SubParticle<SplitOp({3}).data()>>>(
-        n);
+        SubParticle<SplitOp({1, 2}).data()>,
+        SubParticle<SplitOp({3}).data()>>>( n);
     RunAllBenchmarks<PartitionedContainer<
         ParticleRef, SubParticle<SplitOp({0}).data()>,
-        SubParticle<SplitOp({2, 1}).data()>, SubParticle<SplitOp({3}).data()>>>(
-        n);
+        SubParticle<SplitOp({2, 1}).data()>,
+        SubParticle<SplitOp({3}).data()>>>( n);
     RunAllBenchmarks<PartitionedContainer<
         ParticleRef, SubParticle<SplitOp({0, 3}).data()>,
-        SubParticle<SplitOp({1}).data()>, SubParticle<SplitOp({2}).data()>>>(n);
+        SubParticle<SplitOp({1}).data()>,
+        SubParticle<SplitOp({2}).data()>>>(n);
     RunAllBenchmarks<PartitionedContainer<
         ParticleRef, SubParticle<SplitOp({3, 0}).data()>,
-        SubParticle<SplitOp({1}).data()>, SubParticle<SplitOp({2}).data()>>>(n);
+        SubParticle<SplitOp({1}).data()>,
+        SubParticle<SplitOp({2}).data()>>>(n);
     RunAllBenchmarks<PartitionedContainer<
         ParticleRef, SubParticle<SplitOp({0}).data()>,
-        SubParticle<SplitOp({1, 3}).data()>, SubParticle<SplitOp({2}).data()>>>(
-        n);
+        SubParticle<SplitOp({1, 3}).data()>,
+        SubParticle<SplitOp({2}).data()>>>( n);
     RunAllBenchmarks<PartitionedContainer<
         ParticleRef, SubParticle<SplitOp({0}).data()>,
-        SubParticle<SplitOp({3, 1}).data()>, SubParticle<SplitOp({2}).data()>>>(
-        n);
+        SubParticle<SplitOp({3, 1}).data()>,
+        SubParticle<SplitOp({2}).data()>>>( n);
     RunAllBenchmarks<PartitionedContainer<
         ParticleRef, SubParticle<SplitOp({0}).data()>,
-        SubParticle<SplitOp({1}).data()>, SubParticle<SplitOp({2, 3}).data()>>>(
-        n);
+        SubParticle<SplitOp({1}).data()>, SubParticle<SplitOp({2,
+        3}).data()>>>( n);
     RunAllBenchmarks<PartitionedContainer<
         ParticleRef, SubParticle<SplitOp({0}).data()>,
-        SubParticle<SplitOp({1}).data()>, SubParticle<SplitOp({3, 2}).data()>>>(
-        n);
+        SubParticle<SplitOp({1}).data()>, SubParticle<SplitOp({3,
+        2}).data()>>>( n);
     RunAllBenchmarks<PartitionedContainer<
         ParticleRef, SubParticle<SplitOp({0}).data()>,
         SubParticle<SplitOp({1}).data()>, SubParticle<SplitOp({2}).data()>,
