e97ddf844ddc352e62c0eddb51bc68feae744e52
Jolly Chen <jolly.chen@cern.ch>
2026-01-05 13:49:17 +0100

    Define struct and proxy ref struct

--- datastructures.h
+++ datastructures.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f836a523fa9b90328155faef121475278cc949cc6502ed6a1f6a53948bef2f2c
-size 4831
+oid sha256:672ab105a8fe34e756577a7596fce680b7c731d94b6a7e967dd2726ebc2a0803
+size 4913
