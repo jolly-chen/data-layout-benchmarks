d4edda299235e19f14b784de36e64cf224e7ce74
Jolly Chen <jolly.chen@student.uva.nl>
2026-01-09 15:02:58 +0100

    Enable all data members of Particle

--- generate_datastructures.py
+++ generate_datastructures.py
@@ -7,13 +7,13 @@ import os
 import sys
 
 data_members = [
-    # ("int", "id"),
+    ("int", "id"),
     ("double", "pt"),
     ("double", "eta"),
     ("double", "phi"),
     ("double", "e"),
-    # ("char", "charge"),
-    # ("std::array<std::array<double, 3>, 3>", "posCovMatrix"),
+    ("char", "charge"),
+    ("std::array<std::array<double, 3>, 3>", "posCovMatrix"),
 ]
 
 struct_name_base = "Particle"
