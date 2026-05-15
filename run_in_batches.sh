dbda2c449ebacdfdbe6b52472a9bac66bbb950e4
Jolly Chen <jolly.chen@cern.ch>
2026-05-15 22:29:44 +0200

    Update to match what was used for paper
    This script was used to run the benchmarks
    on DAS5, DAS6, and locally where the
    memory capacities were not large enough to
    compile the benchmarks with all layouts.

--- run_in_batches.sh
+++ run_in_batches.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TOTAL_SIZE=226562 # For 7 data members
+TOTAL_SIZE=37633 # For 7 data members
 NUM_BATCHES=4
 BATCH_SIZE=$(( (TOTAL_SIZE + NUM_BATCHES - 1) / (NUM_BATCHES) )) # Ceiling division
 
@@ -8,10 +8,12 @@ INPUT1_FILE=$1
 INPUT2_FILE=$2
 OUTPUT_FILE=$3
 
+cmake . -Dvec=on
+
 for (( batch_num=0; batch_num < NUM_BATCHES; batch_num++ ))
 do
     echo "Running batch $((batch_num + 1)) / $NUM_BATCHES (with batch size $BATCH_SIZE)"
-    python3 generate_datastructures.py --batch_size $BATCH_SIZE --batch_num $batch_num --contiguous
+    python3 generate_datastructures.py --data_spec particle.spec --batch_size $BATCH_SIZE --batch_num $batch_num 
     /usr/bin/time -v make main
     likwid-pin -C 0 ./main --input1 $INPUT1_FILE --input2 $INPUT2_FILE --output $OUTPUT_FILE
 done
